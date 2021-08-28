238af26a866883c69029e9082e8b77b8d3633f39
Felbf <felbf92@gmail.com>
2021-08-28 07:27:39 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domain = [".com", ".net", ".us", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    let pronouns = pronoun[i];
+    for (let j = 0; j < adj.length; j++) {
+      let adjs = adj[j];
+
+      for (let z = 0; z < noun.length; z++) {
+        let nouns = noun[z];
+
+        for (let k = 0; k < domain.length; k++) {
+          let domains = domain[k];
+          console.log(`${pronouns}${adjs}${nouns}${domains}`);
+        }
+      }
+    }
+  }
+  console.log(`${pronoun[0]}${adj[0]}${noun[0]}${domain[0]}`);
 };
